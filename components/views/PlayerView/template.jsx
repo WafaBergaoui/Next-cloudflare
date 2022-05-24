cbb7e8f6497375e817e338d75e9125011c754427
Wafa Bergaoui <wafa.bergaoui@sastec-group.com>
2022-05-24 09:13:30 +0100

    first commit

---
+++ components/views/PlayerView/template.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Stream } from "@cloudflare/stream-react";
+import S from './style.module.scss';
+
+const PlayerView = () => {
+
+  const videoID = "548a1762f17f8ea1cda8344e0e48de7a"
+  return (
+    <div className={S.Player}>
+      <Stream controls src={videoID}/>
+    </div>
+  )
+}
+export default PlayerView
+
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import Head from 'next/head'
+import { PlayerView } from "../components/views";
 
-import Post from '../components/post'
-
-export async function getStaticProps() {
-  // fetch list of posts
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_page=1'
-  )
-  const postList = await response.json()
-  return {
-    props: {
-      postList,
-    },
-  }
-}
-
-export default function IndexPage({ postList }) {
+export default function Home() {
   return (
-    <main>
-      <Head>
-        <title>Home page</title>
-      </Head>
-
-      <h1>List of posts</h1>
-
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
-    </main>
-  )
+    <div>
+      <PlayerView />
+    </div>
+  );
 }
